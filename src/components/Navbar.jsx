1fb7a5d7bd58b14bde786a944d6d213e3158d979
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-07-27 09:48:32 +0530

    hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,23 +4,23 @@ import {navLists} from '../constants'
 const Navbar = () => {
   return (
     <header className='w-full py-5 sm:px-10 px-5 flex justify-between items-center'>
-      <nav className='flex justify-between items-center w-full'>
-        <img className='w-5' src={appleImg} alt="" />
+      <nav className='flex justify-between items-center w-full max-w-screen-xl mx-auto'>
+        <img className='w-5 cursor-pointer' src={appleImg} alt="" />
         
         {/* navitems */}
-        <div className='sm:flex gap-8 items-center hidden'>{navLists.map((nav)=>{
+        <div className='sm:flex items-center hidden'>{navLists.map((nav)=>{
             return(
-                <div className='' key={nav}>
+                <p className='cursor-pointer px-5 text-sm text-gray-300 hover:text-white transition-all' key={nav}>
                     {nav}
-                </div>
+                </p>
             )
         })}</div>
 
 
         {/* nav buttons  */}
         <div className='flex gap-5'>
-            <img src={searchImg} alt="search" width={18} height={18} />
-            <img src={bagImg} alt="bag" width={18} height={18}/>
+            <img className='cursor-pointer w-4' src={searchImg} alt="search" width={18} height={18} />
+            <img className='cursor-pointer w-4' src={bagImg} alt="bag" width={18} height={18}/>
         </div>
       </nav>
     </header>
